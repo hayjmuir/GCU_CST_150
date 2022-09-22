84e850d5f0a1e7d58ff2752f7e2d916b71418d45
hayjmuir <58193716+hayjmuir@users.noreply.github.com>
2022-09-21 17:38:44 -0700

    Add files via upload

---
+++ Assignment2/Form1.cs
@@ -0,0 +1,17 @@
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showAnswerButton_Click(object sender, EventArgs e)
+        {
+            answerLabel.Text = "All of them!";
+        }
+
+    
+    }
+}
