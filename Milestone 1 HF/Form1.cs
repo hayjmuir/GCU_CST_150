33afb7641dcc518134c2cca7020ba3a516dcf646
hayjmuir <58193716+hayjmuir@users.noreply.github.com>
2022-09-21 17:42:42 -0700

    Add files via upload

---
+++ Milestone 1 HF/Form1.cs
@@ -0,0 +1,20 @@
+namespace Milestone_1_HF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Produce Result from search bar");
+        }
+
+        private void reorderButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bring up order form");
+        }
+    }
+}
