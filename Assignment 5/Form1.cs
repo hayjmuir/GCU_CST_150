45d0cbbbf5d437483ed7aaee89195774c413fc55
hayjmuir <58193716+hayjmuir@users.noreply.github.com>
2022-10-03 01:26:26 -0700

    Update Form1.cs

--- Assignment 5/Form1.cs
+++ Assignment 5/Form1.cs
@@ -24,47 +24,7 @@ namespace Assignment_5
         {
             try
             {
-
-                /* I have:
-                 *    A text file
-                 * I want:
-                 *    [X] First word alphabetically
-                 *    [X] Last word alphabetically
-                 *    [ ] Longest word
-                 *    [ ] Word with most vowels
-                 * 
-                 * Get ready to remember the first word, last word, longest word, and word with most vowels
-                 * 
-                 * [X] Open the file
-                 * [X] While there are still words to look at
-                 * [X]   Get the next word
-                 *    
-                 * [X]   Compare the next word to the first word
-                 * [X]   If that word comes before the first word alphabetically
-                 * [X]       Replace the first word with this word
-                 *    
-                 * [X]   Compare the next word to the last word
-                 * [X]   If that word comes before the last word alphabetically
-                 * [X]       Replace the last word with this word
-                 *    
-                 * [X]   Compare the next word to the longest word
-                 * [X]   If that word is longer than the longest word
-                 * [X]       Replace the longest word with this word
-                 * 
-                 * [X]   Compare the next word to the word with the most vowels
-                 * [X]   If that word has more vowels
-                 * [X]       Replace the most vowels word with this word
-                 * 
-                 * [X]   Display all words in lowercase
-                 * 
-                 * [X] Loop
-                 * 
-                 * [X] Display Stats
-                 * [ ] Save stats to file
-                 */
-
-
-
+      
                 string currentWord;
                 string firstWordAlphabetically = string.Empty; 
                 string lastWordAlphabetically = string.Empty;
