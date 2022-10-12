e961c08c9c1d40cede32df6e5e295416e612cfea
hayjmuir <58193716+hayjmuir@users.noreply.github.com>
2022-10-11 22:59:16 -0700

    Add files via upload

---
+++ Debuggin1/Program.cs
@@ -0,0 +1,40 @@
+﻿//use for IC08
+//Lydia's code
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST117_IC08_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
