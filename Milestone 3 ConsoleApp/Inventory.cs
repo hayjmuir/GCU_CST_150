4c82b2ae2ad421a1f56a1fff77a78854aae28d5c
hayjmuir <58193716+hayjmuir@users.noreply.github.com>
2022-10-20 22:39:39 -0700

    Add files via upload

---
+++ Milestone 3 ConsoleApp/Inventory.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_1_HF
+{
+    internal class Item
+    {
+        public static int idcounter;
+        public int id;
+        public string name;
+        public int quantity;
+        public override string ToString() 
+        
+        { return id + "\t" + name + " | " + quantity; }
+
+        public Item(string name, int quantity)
+        {
+            this.id = idcounter++;
+            this.name = name;
+            this.quantity = quantity;
+        }
+    }
+
+
+
+
+    internal class Inventory
+    {
+        List<Item> items;
+      
+        
+            
+        public Inventory()
+        {
+
+            items = new List<Item>();
+
+        }
+
+
+
+        //search by name 
+        public Item? Search(string name)
+        {
+            foreach (Item item in items)
+            {
+                if (item.name == name)
+                    return item;
+
+            }
+            return null;
+
+
+        }
+
+
+        //search by quantity
+        public Item? Search(int quantity)
+        {
+            foreach (Item item in items)
+            {
+                if (item.quantity == quantity)
+                    return item;
+
+            }
+            return null;
+
+
+        }
+
+
+        //adds item to inventory
+        public void addItem(string name, int quantity)
+        {
+            
+            items.Add(new Item(name, quantity));
+
+
+        }
+
+
+        //removes item from inventory
+        public void removeItem(Item item)
+        {
+            items.Remove(item);
+
+
+        }
+
+
+
+        //edits name 
+
+        public void editItem(Item item, string new_name)
+        {
+            item.name = new_name;
+        }
+
+        //edits quantity
+        public void editItem(Item item, int new_quantity)
+        {
+            item.quantity = new_quantity;
+        }
+
+
+        public string ToString()
+        {
+
+            return String.Join("\n" , items);
+
+
+        }
+
+
+    }
+}
---
+++ Milestone 3 ConsoleApp/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Milestone_1_HF
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            Inventory inventory = new Inventory();
+
+            inventory.addItem("Coke", 120);
+            inventory.addItem("Sprite", 55);
+            inventory.addItem("Dr.Pepper", 23);
+            inventory.addItem("Big Red", 22);
+
+            
+
+
+
+            Console.WriteLine(inventory.ToString());
+           
+                                            
+            Console.WriteLine();
+
+            Item item = (Item)inventory.Search("Coke");
+            Console.WriteLine(item);
+            
+            Console.WriteLine();
+
+            inventory.editItem(item, "Diet Coke");
+            Console.WriteLine(inventory.ToString());
+            
+            Console.WriteLine();
+
+            inventory.editItem(item, 200);
+            Console.WriteLine(inventory.ToString());
+
+            Console.WriteLine();
+
+            inventory.removeItem(item);
+            
+            Console.WriteLine(inventory.ToString());
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
