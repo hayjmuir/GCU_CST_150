cd7ac6497a854859955ae0cc9b5c258d9a073d23
hayjmuir <58193716+hayjmuir@users.noreply.github.com>
2022-10-22 23:27:10 -0700

    Add files via upload

---
+++ Assignment 12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class WordChecker
+{
+    public static void Main()
+    {
+        
+        int word, l;
+
+
+        
+        string @string = System.IO.File.ReadAllText("input.txt");
+
+        l = 1;
+        word = 1;
+
+        // loop till end of string 
+        while (l <= @string.Length - 1)
+        {
+            //check whether the current character is white space or new line or tab character
+            if ((@string[l] == '.' || @string[l] == ' ' || @string[l] == '\t' || @string[l] == '!' || @string[l] == ',' || @string[l] == '?' || @string[l] == '\n') && (@string[l - 1] == 't' || @string[l - 1] == 'e'))
+            {
+                word++;
+            }
+
+
+            l++;
+        }
+
+        Console.Write("There are {0} words ending in t or e", word);
+    }
+}
