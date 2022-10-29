da4450b9805cf7a1e179aacbf260a6ca0044d586
hayjmuir <58193716+hayjmuir@users.noreply.github.com>
2022-10-28 18:54:05 -0700

    Add files via upload

---
+++ Milestone 4/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Milestone_1_HF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "",
+            ""}, -1);
+            this.addButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.idNumber = new System.Windows.Forms.ColumnHeader();
+            this.sodaName = new System.Windows.Forms.ColumnHeader();
+            this.quantityCol = new System.Windows.Forms.ColumnHeader();
+            this.addTextBox = new System.Windows.Forms.TextBox();
+            this.quantityBox = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Amount = new System.Windows.Forms.Label();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.quantityBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(542, 158);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(133, 44);
+            this.addButton.TabIndex = 12;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(542, 222);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(133, 44);
+            this.editButton.TabIndex = 13;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(329, 360);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(133, 44);
+            this.searchButton.TabIndex = 14;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.idNumber,
+            this.sodaName,
+            this.quantityCol});
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(57, 34);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(339, 288);
+            this.listView1.TabIndex = 22;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // idNumber
+            // 
+            this.idNumber.Text = "ID";
+            // 
+            // sodaName
+            // 
+            this.sodaName.Text = "Soda";
+            // 
+            // quantityCol
+            // 
+            this.quantityCol.Text = "Quantity";
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Location = new System.Drawing.Point(542, 60);
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.Size = new System.Drawing.Size(133, 23);
+            this.addTextBox.TabIndex = 23;
+            // 
+            // quantityBox
+            // 
+            this.quantityBox.Location = new System.Drawing.Point(542, 114);
+            this.quantityBox.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.quantityBox.Name = "quantityBox";
+            this.quantityBox.Size = new System.Drawing.Size(133, 23);
+            this.quantityBox.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(542, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Name";
+            // 
+            // Amount
+            // 
+            this.Amount.AutoSize = true;
+            this.Amount.Location = new System.Drawing.Point(542, 96);
+            this.Amount.Name = "Amount";
+            this.Amount.Size = new System.Drawing.Size(51, 15);
+            this.Amount.TabIndex = 29;
+            this.Amount.Text = "Amount";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(542, 292);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(133, 44);
+            this.deleteButton.TabIndex = 30;
+            this.deleteButton.Text = "Delete Item";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(57, 372);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(251, 23);
+            this.searchBox.TabIndex = 31;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Search Bar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.Amount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.quantityBox);
+            this.Controls.Add(this.addTextBox);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Inventory";
+            ((System.ComponentModel.ISupportInitialize)(this.quantityBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button addButton;
+        private Button editButton;
+        private Button searchButton;
+        private ListView listView1;
+        private ColumnHeader idNumber;
+        private ColumnHeader sodaName;
+        private ColumnHeader quantityCol;
+        private TextBox addTextBox;
+        private NumericUpDown quantityBox;
+        private Label label1;
+        private Label Amount;
+        private Button deleteButton;
+        private TextBox searchBox;
+        private Label label2;
+    }
+}
---
+++ Milestone 4/Inventory.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_1_HF
+{
+    internal class Item
+    {
+        public static int idcounter;
+        public int id;
+        public string name;
+        public int quantity;
+        public override string ToString()
+
+        { return id + "\t" + name + " | " + quantity; }
+
+        public Item(string name, int quantity)
+        {
+            this.id = idcounter++;
+            this.name = name;
+            this.quantity = quantity;
+        }
+    }
+
+
+
+
+
+    internal class Inventory
+    {
+        public List<Item> items;
+
+
+
+        public Inventory()
+        {
+
+            items = new List<Item>();
+
+        }
+
+
+
+        //search by name 
+        public Item? Search(string name)
+        {
+            foreach (Item item in items)
+            {
+                if (item.name == name)
+                    return item;
+
+            }
+            return null;
+
+
+        }
+
+
+        //search by id
+        public Item? Search(int id)
+        {
+            foreach (Item item in items)
+            {
+                if (item.id == id)
+                    return item;
+
+            }
+            return null;
+
+
+        }
+
+
+        //adds item to inventory
+        public void addItem(string name, int quantity)
+        {
+
+            items.Add(new Item(name, quantity));
+
+
+        }
+
+
+        //removes item from inventory
+        public void removeItem(Item item)
+        {
+            items.Remove(item);
+
+
+        }
+
+
+
+        //edits name 
+
+        public void editItem(Item item, string new_name)
+        {
+            item.name = new_name;
+        }
+
+        //edits quantity
+        public void editItem(Item item, int new_quantity)
+        {
+            item.quantity = new_quantity;
+        }
+
+
+        public string ToString()
+        {
+
+            return String.Join("\n", items);
+
+
+        }
+
+
+    }
+}
